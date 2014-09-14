5444bf8ea4f9519776a501f3d937ef2fc56fbc1b
lindamariemummy <lindamariemummy@gmail.com>
2014-09-13 20:29:28 -0700

    initial upload of racing game

---
+++ button.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  
+  //store reference to button in variable
+  var $b = $("#button");
+    $b.mouseenter(function() {
+        $b.fadeTo("fast", .8);
+    });
+    $b.mouseleave(function() {
+        $b.fadeTo("fast", 1);
+    });
+    $b.click(function(){
+    	
+        //click only advances the animals if no one has won
+        if(!winnerExists) {
+            winnerExists = timeStep();
+        }
+    });
+});
